76e3b1d51b12b032065e558dc9a596ad602cd265
Alisher Zhumakhmet <alisher.zhumakhmet@nelo-solutions.com>
2019-06-09 11:46:59 +0600

    biography

--- src/app/home/subpages/biography/biography.component.ts
+++ src/app/home/subpages/biography/biography.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-biography',
   templateUrl: './biography.component.html',
   styleUrls: ['./biography.component.scss']
 })
-export class BiographyComponent implements OnInit {
+export class BiographyComponent implements OnInit, AfterViewInit {
+
+    navItem = 1;
 
   constructor() { }
 
   ngOnInit() {
+      window.scrollTo(0, 0);
+  }
+
+  ngAfterViewInit(): void {
+      $('#pills-tab a').on('click', function (e) {
+          console.log('aaaa');
+          e.preventDefault();
+          $(this).tab('show');
+      });
   }
 
 }
