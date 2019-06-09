931be3b78dd611bee3cdbd7cc87713d2b6131c4c
Alisher Zhumakhmet <alisher.zhumakhmet@nelo-solutions.com>
2019-06-09 09:25:20 +0600

    sportsman page

--- src/app/home/sportsman-rating-page/sportsman-rating-page.component.ts
+++ src/app/home/sportsman-rating-page/sportsman-rating-page.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SportsmanRatingPageComponent implements OnInit {
 
+    page = 1;
+    pageSize = 7
+
 
   constructor() { }
 
--- src/app/home/sportsman-rating-page/sportsman-rating-page.module.ts
+++ src/app/home/sportsman-rating-page/sportsman-rating-page.module.ts
@@ -5,12 +5,14 @@ import { SportsmanRatingPageRoutingModule } from './sportsman-rating-page-routin
 import { SportsmanRatingPageComponent } from './sportsman-rating-page.component';
 import { UsersService } from '../../shared/services/user.service';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [SportsmanRatingPageComponent],
   imports: [
     CommonModule,
     SportsmanRatingPageRoutingModule,
+      NgbPaginationModule
   ],
   providers: [
       UsersService,
